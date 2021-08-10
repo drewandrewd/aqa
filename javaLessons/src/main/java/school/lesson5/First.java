abf6c1abcbe6e90cbf4105b47b00f8225fea7214
drewandrewd <andrewid10@gmail.com>
2021-08-11 01:10:53 +0500

    new

--- javaLessons/src/main/java/school/lesson5/First.java
+++ javaLessons/src/main/java/school/lesson5/First.java
@@ -16,17 +16,16 @@ public class First<T> {
         this.nums = nums;
     }
 
-    public T[] changeArr(int firstIndex, int secondIndex) {
+    public void changeArr(int firstIndex, int secondIndex) {
         T one = getNums()[firstIndex];
         T two = getNums()[secondIndex];
         getNums()[firstIndex] = two;
         getNums()[secondIndex] = one;
-        return nums;
     }
 
     public static void main(String[] args) {
         First<Integer> nums = new First<>(new Integer[] {5, 20, 30, 40});
-        nums.changeArr(0, 1);
+        nums.changeArr(0, 3);
         for (int i = 0; i < nums.getNums().length; i++) {
             System.out.print(nums.getNums()[i] + " ");
         }
