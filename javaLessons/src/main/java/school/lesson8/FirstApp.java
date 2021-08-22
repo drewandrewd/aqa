086dace18e0e8da13bd33c33fed0cb7d13c4b499
drewandrewd <andrewid10@gmail.com>
2021-08-22 21:57:51 +0500

    new

--- javaLessons/src/main/java/school/lesson8/FirstApp.java
+++ javaLessons/src/main/java/school/lesson8/FirstApp.java
@@ -1,7 +1,6 @@
 package school.lesson8;
 
-import java.util.ArrayList;
-import java.util.Arrays;
+import java.util.*;
 
 public class FirstApp {
 
@@ -20,24 +19,27 @@ public class FirstApp {
     }
 
     public void removeCopies() {
-        ArrayList<String> newList = new ArrayList<>();
-        for (String curr : list) {
-            for (String newCurr : list) {
-                if (list.indexOf(curr) != list.indexOf(newCurr)) {
-                    continue;
-                } else if (!curr.equals(newCurr)) {
-                    newList.add(newCurr);
-                }
-            }
-        }
+        Set<String> newList = new HashSet<String>(getList());
         for (String curr : newList) {
             System.out.println(curr);
+        }
+    }
+
+    public void countSameWords() {
+        Map<String, Integer> newMap = new HashMap<>();
+        for (String curr : getList()) {
+            int count = newMap.getOrDefault(curr, 0);
+            newMap.put(curr, count + 1);
+        }
+        for (Map.Entry<String, Integer> pair : newMap.entrySet()) {
+            System.out.println(pair.getKey() + ": " + pair.getValue());
         }
     }
 
     public static void main(String[] args) {
         FirstApp app = new FirstApp(new ArrayList<String>(Arrays.asList("apple", "berry", "orange", "banana", "pineapple", "berry", "watermelon", "apple", "lemon", "lemon", "orange")));
         app.removeCopies();
-
+        System.out.println();
+        app.countSameWords();
     }
 }
